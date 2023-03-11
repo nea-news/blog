19ca24c8d73e0590824085f313c5a85c4291e0c7
v1centebarros <vmabarros@ua.pt>
2023-03-11 23:32:34 +0000

    update site urls to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nea-news.github.io/blog",
+  site: "https://nea-news.github.io/",
   base: "/blog",
   integrations: [
     tailwind({
